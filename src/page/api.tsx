05445b8b8e4418ba73bf11f797deb550106b52ba
Suyi <thonatos.yang@gmail.com>
2022-01-04 21:29:13 +0800

    feat: pages (#8)
    * feat: site config
    
    * feat: add pages

--- src/page/api.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const API: React.FC<Props> = (props) => {
-  return <div>api</div>;
-};
-
-export default API;
-
-interface Props {}
---
+++ src/page/api/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ApiPage: React.FC<Props> = (props) => {
+  return <div>TODO.</div>;
+};
+
+export default ApiPage;
+
+interface Props {}
