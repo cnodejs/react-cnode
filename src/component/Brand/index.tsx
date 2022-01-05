3acef3b31e83d7bf402ec0b1b1d0bf2d02fbb649
Suyi <thonatos.yang@gmail.com>
2022-01-05 20:44:02 +0800

    feat: layout (#13)
    * feat: update basic layout
    
    * fix: reset form after submit
    
    * feat: add message page
    
    * feat: refactor topic list

--- src/component/Brand/index.tsx
+++ src/component/Brand/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RouteContext } from '@ant-design/pro-layout';
+
 import * as styles from './index.less';
 
-const Brand: React.FC<Props> = ({ logo, title, description }) => (
-  <div className={styles.container}>
-    <img className={styles.logo} src={logo} alt="logo" />
-    {/* <h1 className={styles.title}>{title}</h1> */}
-    <p className={styles.description}>{description}</p>
-  </div>
-);
+const Brand: React.FC<Props> = ({ logo, title, description }) => {
+  const { collapsed, isMobile } = useContext(RouteContext);
+
+  return (
+    <div className={styles.container}>
+      <img className={styles.logo} src={logo} alt="logo" />
+      {collapsed || isMobile ? null : (
+        <p className={styles.description}>{description}</p>
+      )}
+    </div>
+  );
+};
 
 export default Brand;
 
 interface Props {
-  title: string;
-  description: string;
   logo?: string;
+  title?: string;
+  description: string;
 }
