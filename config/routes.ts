9d7a7bfdd720376263c5f513c0544729400ac6c1
JASONPANGGO <jason_pang@126.com>
2022-01-08 02:52:00 +0800

    refactor: rename topic create to edit

--- config/routes.ts
+++ config/routes.ts
@@ -54,7 +54,7 @@ const routes: IRoute[] = [
         path: '/topic/create',
         exact: true,
         title: '新建话题',
-        component: '@/page/topic/create',
+        component: '@/page/topic/edit',
         access: 'canPostTopic',
       },
       {
@@ -65,7 +65,7 @@ const routes: IRoute[] = [
       {
         path: '/topic/:id/edit',
         exact: true,
-        component: '@/page/topic/create',
+        component: '@/page/topic/edit',
       },
       {
         path: '/user/:loginname',
